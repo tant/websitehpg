30b3861350155cb740ce9ae3d80d5172ad9c2dd9
Tan Tran <2188362+tant@users.noreply.github.com>
2025-03-07 12:22:22 +0700

    first version

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hayphagia.vn</h1>
-      
+    <div className="bg-indigo-100">
+    <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+      <h2 className="max-w-2xl text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+        Minh Hồ đang bắt đầu lại...<br />
+        
+      </h2>
+      <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
+        <a
+          href="mailto:hello@hayphagia.vn"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Liên hệ
+        </a>
+        
+      </div>
     </div>
+  </div>
   );
 }
