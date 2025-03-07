b3e3e9017d5e29756c5e0e25202cf1b627e879de
Tan Tran <2188362+tant@users.noreply.github.com>
2025-03-07 12:24:40 +0700

    chinh lai gmail truoc da

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </h2>
       <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
         <a
-          href="mailto:hello@hayphagia.vn"
+          href="mailto:tiemmaytinhchuminh@gmail.com"
           className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Liên hệ
